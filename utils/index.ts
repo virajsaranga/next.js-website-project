2e9f9d0bc1db626442e5c94cb3aaec4d11c21ac1
virajsaranga <virajridi@gmail.com>
2024-12-27 23:26:55 +0530

    Append the required query parameters to the URL

--- utils/index.ts
+++ utils/index.ts
@@ -68,6 +68,7 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   const url = new URL("https://cdn.imagin.studio/getimage");
   const { make, model, year } = car;
 
+  // Append the required query parameters to the URL
   url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
   url.searchParams.append('make', make);
   url.searchParams.append('modelFamily', model.split(" ")[0]);
