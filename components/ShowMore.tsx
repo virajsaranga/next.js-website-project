774ea9ac0f19ac9288628108fb7080e27ef7cc39
virajsaranga <virajridi@gmail.com>
2025-01-05 19:47:17 +0530

    search bar updated

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -13,7 +13,7 @@ const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
   const handleNavigation = () => {
     // Calculate the new limit based on the page number and navigation type
     const newLimit = (pageNumber + 1) * 10;
-
+//update search params
     const newPathname = updateSearchParams("limit", `${newLimit}`);
     
     router.push(newPathname);
