6bb6c5e45db4cfaf548a4684a4c91f111a50772d
virajsaranga <virajridi@gmail.com>
2025-01-05 19:46:49 +0530

    nav bar update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 //custom button add
 import CustomButton from "./CustomButton";
 
+
 const NavBar = () => (
   <header className='w-full  absolute z-10'>
     <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent'>
