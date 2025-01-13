c2b0de93bb5df7704f8a1078dee44eb5ca6edfc5
virajsaranga <virajridi@gmail.com>
2025-01-13 23:00:29 +0530

    updated

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -12,7 +12,7 @@ export default function CustomFilter({ title, options }: CustomFilterProps) {
   const router = useRouter();
   const [selected, setSelected] = useState(options[0]); // State for storing the selected option
 
-  // update the URL search parameters and navigate to the new URL
+ 
   const handleUpdateParams = (e: { title: string; value: string }) => {
     const newPathName = updateSearchParams(title, e.value.toLowerCase());
 
